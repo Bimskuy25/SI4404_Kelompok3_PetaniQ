2393a77b3f63dfbee479a3b20ace97e1ddc27267
Wakhid Yusuf <wakhidyusuf.2002@gmail.com>
2022-12-26 21:58:25 +0700

    UpdateViewFooter
    Fix the design

--- petaniq/resources/views/konsultasi.blade.php
+++ petaniq/resources/views/konsultasi.blade.php
@@ -19,11 +19,11 @@
     </div>
 </section>
 
-    {{-- footer --}}
-    <div class="text-center p-3 text-dark" style="background-color: #238E68;">
-        © Copyright 2022 PetaniQ
-        <br>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Justo, habitant aliquet proin id volutpat. Est suscipit augue donec laoreet nibh odio volutpat quam.
-    </div>
+{{-- footer --}}
+<div class="text-center p-3 text-light fixed-bottom" style="background-color: #238E68;">
+    © Copyright 2022 PetaniQ
+    <br>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Justo, habitant aliquet proin id volutpat. Est suscipit augue donec laoreet nibh odio volutpat quam.
+</div>
 
 @endsection
