52c106cf5e64510d08d76b337de8367cf0970f50
Wakhid Yusuf <wakhidyusuf.2002@gmail.com>
2022-12-28 17:02:47 +0700

    Fix Detail View
    Fix footer positioning

--- petaniq/resources/views/detail.blade.php
+++ petaniq/resources/views/detail.blade.php
@@ -39,11 +39,12 @@
             </div>
         </div>
 
-        {{-- Footer --}}
-    <div class="text-center p-3 text-light" style="background-color: #238E68;">
-        © Copyright 2022 PetaniQ
-        <br>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Justo, habitant aliquet proin id volutpat. Est suscipit augue donec laoreet nibh odio volutpat quam.
-    </div>
+
 </section>
+{{-- Footer --}}
+<div class="text-center p-3 text-light" style="background-color: #238E68;">
+    © Copyright 2022 PetaniQ
+    <br>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Justo, habitant aliquet proin id volutpat. Est suscipit augue donec laoreet nibh odio volutpat quam.
+</div>
 @endsection
