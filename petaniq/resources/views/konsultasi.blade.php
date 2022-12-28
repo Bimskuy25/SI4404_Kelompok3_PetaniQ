cbbc28a8fe631abf31332c5773aa53943c049f2f
Bimo Agung Fahrizky <cagexx008@gmail.com>
2022-12-28 10:15:11 +0700

    fix footer konsultasi

--- petaniq/resources/views/konsultasi.blade.php
+++ petaniq/resources/views/konsultasi.blade.php
@@ -20,7 +20,7 @@
 </section>
 
 {{-- footer --}}
-<div class="text-center p-3 text-light fixed-bottom" style="background-color: #238E68;">
+<div class="text-center p-3 text-light" style="background-color: #238E68;">
     © Copyright 2022 PetaniQ
     <br>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Justo, habitant aliquet proin id volutpat. Est suscipit augue donec laoreet nibh odio volutpat quam.
