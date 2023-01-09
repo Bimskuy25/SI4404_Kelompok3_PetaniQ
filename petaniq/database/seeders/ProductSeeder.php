940b6928e7f9785f615623ab6ae21c6716395b03
Eccendetiast <ghofarw4@gmail.com>
2023-01-09 07:13:11 +0700

    seeder fix

--- petaniq/database/seeders/ProductSeeder.php
+++ petaniq/database/seeders/ProductSeeder.php
@@ -21,7 +21,7 @@ class ProductSeeder extends Seeder
             'nama_product' => 'Beras Enak',
             'harga' => 15600,
             'deskripsi' => 'Beras unggul kami juga memiliki nilai gizi yang tinggi, sehingga cocok untuk anda yang ingin menjaga kesehatan keluarga anda. Jangan ragu untuk membeli beras unggul kami sekarang juga!',
-            'foto_product' => 'berasEnak.png',
+            'foto_product' => 'rice.jpg',
             'kategori' => 'Beras',
         ]);
         Product::create([
@@ -30,7 +30,7 @@ class ProductSeeder extends Seeder
             'nama_product' => 'Tomat Segar',
             'harga' => 12300,
             'deskripsi' => 'Tomat segar kami juga memiliki nilai gizi yang tinggi, kaya akan vitamin C dan potassium. Jangan ragu untuk membeli tomat segar kami sekarang juga untuk menjaga kesehatan anda dan keluarga anda!',
-            'foto_product' => 'TomatSegar.png',
+            'foto_product' => 'Tomat.jpg',
             'kategori' => 'Sayur',
         ]);
         Product::create([
@@ -39,7 +39,7 @@ class ProductSeeder extends Seeder
             'nama_product' => 'Semangka Klaten',
             'harga' => 13400,
             'deskripsi' => 'Tomat segar kami juga memiliki nilai gizi yang tinggi, kaya akan vitamin C dan potassium. Jangan ragu untuk membeli tomat segar kami sekarang juga untuk menjaga kesehatan anda dan keluarga anda!',
-            'foto_product' => 'SemangkaKlaten.png',
+            'foto_product' => 'Semangka.jpg',
             'kategori' => 'Buah',
         ]);
     }
